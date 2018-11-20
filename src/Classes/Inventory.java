44a346241d3afd814e78f7e5b2b04b0f349f2e58
VarshDagon <julien.valentin@epsi.fr>
2018-11-20 12:18:13 +0100

    Mise en place de l'inventaire

--- src/Classes/Inventory.java
+++ src/Classes/Inventory.java
@@ -1,4 +1,19 @@
 package Classes;
 
 public class Inventory {
+
+    int mousquetons = 15;
+    int gantinter = 10;
+    int ceintureTactiques = 20;
+    int metauxdetector = 25;
+    int securebrassards = 30;
+    int flashlight = 5;
+    int bandeaux = 5;
+    int kevlar = 12;
+    int chemises = 30;
+    int blousons = 30;
+    int windcoat = 30;
+    int talkies = 20;
+    int oreillettes = 10;
+    int tasers = 5;
 }
